ac8a8176f4051f8c6e5f8c5a2a7602365b531526
Hlebets <glib.kobryn@gmail.com>
2023-09-28 21:11:03 +0300

    Created If Else

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ print("Лабораторна робота №2 Варіант 3")
 
 print("Гліб Кобрин, ІКСМ-1, Навчальна Група №2")
 
-num = int(input("Введіть довільне ціле число: "))
+user_input = input("Введіть довільне ціле число: ")
 
-num_str = str(num)
+if user_input.isdigit():
+    num = int(user_input)
 
-for digit in num_str:
-    count = int(digit)
-    histogram = "#" * count
-    print(histogram)
+    num_str = str(num)
+
+    for digit in num_str:
+        count = int(digit)
+        histogram = "#" * count
+        print(histogram)
+
+else:
+    print("Error: Введене значення не є цілим числом")
