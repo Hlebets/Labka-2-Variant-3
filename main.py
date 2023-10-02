b7956dc320ca63e34412389766df318e4d149376
Hlebets <glib.kobryn@gmail.com>
2023-10-02 20:47:35 +0300

    Check

--- main.py
+++ main.py
@@ -2,31 +2,38 @@ print("Лабораторна робота №2 Варіант 3")
 
 print("Гліб Кобрин, ІКСМ-1, Навчальна Група №2")
 
-user_input = input("Введіть довільне ціле число: ")
+while True:
+    user_input = input("Введіть довільне ціле число: ")
 
-if user_input.isdigit():
-    num = int(user_input)
+    if user_input.isdigit():
+        num = int(user_input)
 
-    num_str = str(num)
+        num_str = str(num)
 
-    for digit in user_input:
-        count = int(digit)
-        histogram = "#" * count
+        for digit in user_input:
+            count = int(digit)
+            histogram = "#" * count
 
-        print(histogram)
+            print(histogram)
 
-    for index, digit in enumerate(user_input, start=1):
-        count = int(digit)
-        explanation = "Пояснення: \n"f"Розряд {index}: "
+        explanation_title = "\nПояснення:"
 
-        if count == 1:
-            explanation += f"{count} символ"
-        elif 1 < count < 5:
-            explanation += f"{count} символи"
-        else:
-            explanation += f"{count} символів"
+        print(explanation_title)
 
-        print(explanation)
+        for index, digit in enumerate(user_input, start=1):
+            count = int(digit)
+            explanation = f"Розряд {index}: "
 
-else:
-    print("Error: Введене значення не є цілим числом.")
+            if count == 1:
+                explanation += f"{count} символ"
+            elif 1 < count < 5:
+                explanation += f"{count} символи"
+            else:
+                explanation += f"{count} символів"
+
+            print(explanation)
+
+        break
+
+    else:
+        print("Error: Введене значення не є цілим числом.")
